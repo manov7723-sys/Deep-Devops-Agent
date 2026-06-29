987997d5442caaf9dd9a9900b0998f650b0d1082
manov7723-sys <manov7723@gmail.com>
2026-06-29 21:49:01 +0530

    Delete prisma directory

--- prisma/migrations/20260619120000_add_cloudprovider_credvaultpath/migration.sql
+++
@@ -1,3 +0,0 @@
--- Add a non-secret pointer to the HashiCorp Vault path that holds a cloud
--- provider's AWS access key + secret. Secrets live in Vault, not Postgres.
-ALTER TABLE "CloudProvider" ADD COLUMN "credVaultPath" TEXT;
