25629140ad85278a8ec1146fa4b5825316bf2c3c
manov7723-sys <manov7723@gmail.com>
2026-07-07 18:39:20 +0530

    Add Proxmox cluster git-vm (Terraform)

---
+++ terraform/proxmox/git-vm/terraform/proxmox/git-vm/vm.tf
@@ -0,0 +1,48 @@
+resource "proxmox_virtual_environment_vm" "git_vm" {
+  name      = "git-vm"
+  node_name = "pve"
+
+  clone {
+    vm_id = 9000
+    full  = true
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    interface    = "scsi0"
+    size         = 20
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  agent {
+    enabled = true
+  }
+}
+
+output "vm_id" {
+  value = proxmox_virtual_environment_vm.git_vm.vm_id
+}
+
+output "vm_name" {
+  value = proxmox_virtual_environment_vm.git_vm.name
+}
